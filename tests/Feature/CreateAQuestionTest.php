4d99c8d6d1945f45f87edbbedb52f9c57a0b203c
marcosrnuness <marcos@marcos.inf.br>
2023-12-11 21:02:22 -0300

    Hey-11: it should be able to create a new question bigger than 255 characters

---
+++ tests/Feature/CreateAQuestionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\post;
+
+it('should be able to create a new question bigger than 255 characters', function () {
+    // Arrange | Preparar
+    $user = User::factory()->create();
+    actingAs($user);
+
+    // Act | Agir
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 260) . '?',
+    ]);
+
+    // Assert | Verificar
+    $request->assertRedirect(route('dashboard'));
+    assertDatabaseCount('questions', 1);
+    assertDatabaseHas('questions', [
+        'question' => str_repeat('*', 260) . '?',
+    ]);
+});
+
+it('should check if ends woth question mark?', function () {
+
+})->todo();
+
+it('should have at least 10 characters', function () {
+
+});
