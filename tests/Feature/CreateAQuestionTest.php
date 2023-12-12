6150785250be264375d1597d8373b6dc5a168806
marcosrnuness <marcos@marcos.inf.br>
2023-12-11 22:15:45 -0300

    Hey-11: it should have at least 10 characters pint

--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -1,10 +1,8 @@
 <?php
 
 use App\Models\User;
-use function Pest\Laravel\actingAs;
-use function Pest\Laravel\assertDatabaseCount;
-use function Pest\Laravel\assertDatabaseHas;
-use function Pest\Laravel\post;
+
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
 
 it('should be able to create a new question bigger than 255 characters', function () {
     // Arrange | Preparar
@@ -36,7 +34,7 @@ it('should check if ends woth question mark?', function () {
 
     // Assert | Verificar
     $request->assertSessionHasErrors([
-        'question' => 'Are you sure that is a question? It is missing the question mark in the end.'
+        'question' => 'Are you sure that is a question? It is missing the question mark in the end.',
     ]);
     assertDatabaseCount('questions', 0);
 });
