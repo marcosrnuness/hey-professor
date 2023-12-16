46b052e03b7bad44f610577fc0f7253457ab9640
marcosrnuness <marcos@marcos.inf.br>
2023-12-16 11:19:54 -0300

    Hey-11: being able to see the list of questions in the dashboar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,5 +17,15 @@
                 Cancel
             </x-button.reset>
         </x-form>
+
+        <hr class="border-gray-700 border-dashed my-4">
+
+        <div class="dark:text-gray-300 uppercase font-bold mb-1">List of Question</div>
+
+        <div class="dark:text-gray-400 space-y-4">
+            @foreach($questions as $item)
+                <x-question :question="$item"/>
+            @endforeach
+        </div>
     </x-container>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\{ProfileController, QuestionController};
+use App\Http\Controllers\{DashboardController, ProfileController, QuestionController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,9 +15,7 @@ Route::get('/', function () {
 
 Route::post('/question/store', [QuestionController::class, 'store'])->name('question.store');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
