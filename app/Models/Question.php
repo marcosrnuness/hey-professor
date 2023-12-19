16ed00db3d38fe4b57c2f02313d013138ce0d646
marcosrnuness <marcos@marcos.inf.br>
2023-12-19 18:16:13 -0300

    Hey-11: handling N+1 issue

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -21,13 +20,13 @@ class Question extends Model
 
     protected $guarded = [];
 
-    public function likes(): Attribute
-    {
-        return new Attribute(fn () => $this->votes()->sum('like'));
-    }
+    //    public function likes(): Attribute
+    //    {
+    //        return new Attribute(fn () => $this->votes()->sum('like'));
+    //    }
 
-    public function unlikes(): Attribute
-    {
-        return new Attribute(fn () => $this->votes()->sum('unlike'));
-    }
+    //    public function unlikes(): Attribute
+    //    {
+    //        return new Attribute(fn () => $this->votes()->sum('unlike'));
+    //    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\{DashboardController,
+use App\Http\Controllers\{
+    DashboardController,
     ProfileController,
+    QuestionController,
     Question\LikeController,
-    Question\UnlikeController,
-    QuestionController};
+    Question\UnlikeController
+};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
